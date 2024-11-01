48a0895f5bf99edbac56e787768ddca4e14e6ef3
NeilPatilBooth <npatil@ChicagoBooth.edu>
2024-11-01 00:08:25 +0000

    homepage works

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  get ("/"), { :controller => "home", :action =>"homepage"}
+  
   get ("/dice/2/6"), { :controller => "twodsix", :action =>"roll_two_d_six"}
 
 end
